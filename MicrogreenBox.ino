4a91b1d7799973eff0eb7377132e9d4247b17135
Paul VandenBosch <skyl4rk@gmail.com>
2019-04-21 12:08:07 -0400

    Added lighting control and RelayTimer object

--- MicrogreenBox.ino
+++ MicrogreenBox.ino
@@ -1,346 +1,147 @@
-// Pump Timer with Sensor Control and Morse Readout
-// Capacitive Soil Moisture Sensor with AOUT to pin 1
-// Pump will turn off if soil becomes moist during pumping.
-// Exhaust fan thermostat based on tray temp sensor
-// Circulation fan runs when exhaust fan is off
-// Heat mats thermostat based on air temp sensor
-// Paul VandenBosch, 20190220
 
-#define HEATSET 320     // Sensor value to turn on heater below this temperature setting
-#define AIRTEMPSET 310  // Sensor value to turn on exhaust fan above this temperature setting
-#define AIRTEMPOFFSET 5 // Circulation fan will start x number below AIRTEMPSET for exhaust fan
-#define DRYSOILSET 460  // Moisture sensor Value that triggers watering, soil is dry at this value
-#define WETSOILSET 400  // Moisture sensor Value that shuts off water pump
-#define EXHAUSTFANCYCLETIME 600000 // ms between exhaust fan recurring runs
-#define EXHAUSTFANDELAY 6000 // ms to run exhaust fan on recurring runs
+#define PUMPONTIME 60000         // Run pump for X milliseconds - 90 seconds is 90000
+#define PUMPOFFTIME 21600000     // Pause pump for X milliseconds - 6 hours is 21600000
+#define LIGHTONTIME 57600000     // Run light for X milliseconds - 16 hours is 57600000
+#define LIGHTOFFTIME 28800000    // Pause light for X milliseconds - 8 hours is 28800000
+#define LIGHTSTARTTIME 432000000 // Wait X milliseconds before starting light - 5 days is 432000000
+#define HEATSETPOINT 355         // Thermistor setting below which heater is turned on
+#define HYSTERESIS 10             // FANSETPOINT + HYSTERESIS is when the heat turns off and the fan turns on
 
-#define WATERPUMPTIME 60   // Seconds to run water pump for each watering event
-#define FIRSTWATERING 5    // Day after start for the first watering event
-#define SECONDWATERING 40  // Day after start for the first watering event
-#define THIRDWATERING 40   // Day after start for the first watering event
-#define OVERFLOWDAYS 40    // Days after which the crop will be completed
+#define HEATRELAYPIN 4           // Heat relay pin number
+#define FANRELAYPIN 5            // Fan relay pin number
+#define LIGHTRELAYPIN 6          // Light relay pin number
+#define PUMPRELAYPIN 7           // Pump relay pin number
 
-#define HEATRELAYPIN 4  // Pin used for heat relay  
-#define CIRCRELAYPIN 5  // Pin used for circulation fan relay
-#define FANRELAYPIN 6   //  Pin used for fan relay
-#define PUMPRELAYPIN 7  // Pin used for pump relay
+#define BUTTONPIN 12             // Button grounds out this pin, NOTE: pin mode is INPUT_PULLUP
+#define THERMISTORPIN 0          // Thermistor pin number, put thermistor at seed level in sunflower tray
+                                 // Note: thermistor is connected to 3.3V and with 10k ohm resistor to ground
 
-#define TRAYSENSORPIN 0 // Pin used for heat sensor on tray at seed level
-#define BOXSENSORPIN 2  // Pin used for heat sensor in box for air temperature
-#define MOISTUREPIN 1   // Analog pin used for moisture sensor
-#define BUTTONPIN 12    // Pin used to start manual pump by grounding it
-#define UNUSEDBUTTONPIN 11  // Pin button available for use
+// Declare the class RelayTimer
 
-int moistureSensor = analogRead(MOISTUREPIN);
-int heatSensor = analogRead(TRAYSENSORPIN);
-int airTempSensor = analogRead(BOXSENSORPIN);
-int fan1Toggle = 0;
-
-unsigned long dayMs = 86400000;
-unsigned long hourMs = 3600000;
-unsigned long minuteMs = 60000;
-
-unsigned long waterPumpTime = WATERPUMPTIME * 1000.;
-unsigned long firstWatering = FIRSTWATERING * dayMs;
-unsigned long secondWatering = SECONDWATERING * dayMs; 
-unsigned long thirdWatering = THIRDWATERING *dayMs;
-
-unsigned long currentMillis;
-unsigned long recurringFanStart = EXHAUSTFANCYCLETIME;
-
-
-// Simple Arduino Morse Beacon
-// ****** Morse Beacon Begins ******
-
-#define N_MORSE  (sizeof(morsetab)/sizeof(morsetab[0]))
-#define SPEED  (12)
-#define DOTLEN  (1200/SPEED)
-#define DASHLEN  (4*(1200/SPEED))
-
-int morseLEDpin = 13 ;
-
-struct t_mtab {
-  char c, pat;
-} ;
-
-struct t_mtab morsetab[] = {
-  {'.', 106},
-  {',', 115},
-  {'?', 76},
-  {'/', 41},
-  {'A', 6},
-  {'B', 17},
-  {'C', 21},
-  {'D', 9},
-  {'E', 2},
-  {'F', 20},
-  {'G', 11},
-  {'H', 16},
-  {'I', 4},
-  {'J', 30},
-  {'K', 13},
-  {'L', 18},
-  {'M', 7},
-  {'N', 5},
-  {'O', 15},
-  {'P', 22},
-  {'Q', 27},
-  {'R', 10},
-  {'S', 8},
-  {'T', 3},
-  {'U', 12},
-  {'V', 24},
-  {'W', 14},
-  {'X', 25},
-  {'Y', 29},
-  {'Z', 19},
-  {'1', 62},
-  {'2', 60},
-  {'3', 56},
-  {'4', 48},
-  {'5', 32},
-  {'6', 33},
-  {'7', 35},
-  {'8', 39},
-  {'9', 47},
-  {'0', 63}
-} ;
-
-void
-dash()
+class RelayTimer
 {
-  digitalWrite(morseLEDpin, HIGH) ;
-  delay(DASHLEN);
-  digitalWrite(morseLEDpin, LOW) ;
-  delay(DOTLEN) ;
-}
+  // Class Member Variables
+  int relayPin;    // the number of the relay pin
+  unsigned long OnTime;     // milliseconds of on-time
+  unsigned long OffTime;    // milliseconds of off-time
 
-void
-dit()
-{
-  digitalWrite(morseLEDpin, HIGH) ;
-  delay(DOTLEN);
-  digitalWrite(morseLEDpin, LOW) ;
-  delay(DOTLEN);
-}
+  // These maintain the current state
+  int relayState;                 // relayState used to set the relay 
+  unsigned long relayPreviousMillis;   // will store last time relay was updated
 
-void
-send(char c)
-{
-  int i ;
-  if (c == ' ') {
-    Serial.print(c) ;
-    delay(7 * DOTLEN) ;
-    return ;
+  // Constructor - creates a RelayTimer 
+  // and initializes the member variables and state
+  public:
+  RelayTimer(int pin, unsigned long on, unsigned long off)
+  {
+  relayPin = pin;
+  pinMode(relayPin, OUTPUT);     
+    
+  OnTime = on;
+  OffTime = off;
+  
+  relayState = LOW; 
+  relayPreviousMillis = 0;
   }
-  for (i = 0; i < N_MORSE; i++) {
-    if (morsetab[i].c == c) {
-      unsigned char p = morsetab[i].pat ;
-      Serial.print(morsetab[i].c) ;
 
-      while (p != 1) {
-        if (p & 1)
-          dash() ;
-        else
-          dit() ;
-        p = p / 2 ;
-      }
-      delay(2 * DOTLEN) ;
-      return ;
+  // Member Function:
+  void Update()
+  {
+    // check to see if it's time to change the state of the relay
+    unsigned long currentMillis = millis();
+     
+    if((relayState == HIGH) && (currentMillis - relayPreviousMillis >= OnTime))
+    {
+      relayState = LOW;  // Turn it off
+      relayPreviousMillis = currentMillis;  // Remember the time
+      digitalWrite(relayPin, relayState);  // Update the actual relay pin
+    }
+    else if ((relayState == LOW) && (currentMillis - relayPreviousMillis >= OffTime))
+    {
+      relayState = HIGH;  // turn it on
+      relayPreviousMillis = currentMillis;   // Remember the time
+      digitalWrite(relayPin, relayState);   // Update the actual relay pin
+      Serial.print("Pin On: ");Serial.println(relayPin);
     }
   }
-  /* if we drop off the end, then we send a space */
-  Serial.print("?") ;
-}
+};
 
-void
-sendmsg(char *str)
+// Create instances of the class RelayTimer:
+RelayTimer pump(PUMPRELAYPIN, PUMPONTIME, PUMPOFFTIME);
+RelayTimer light(LIGHTRELAYPIN, LIGHTONTIME, LIGHTOFFTIME);
+
+void setup()
 {
-  while (*str)
-    send(*str++) ;
-  Serial.println("");
-}
-// ****** Morse Beacon Ends ******
-
-
-
-void setup() {
   Serial.begin(9600);
-  
+  pinMode(THERMISTORPIN, INPUT);
   pinMode(BUTTONPIN, INPUT_PULLUP);
-  pinMode(UNUSEDBUTTONPIN, INPUT_PULLUP);
-  pinMode(MOISTUREPIN, INPUT);
-  pinMode(TRAYSENSORPIN, INPUT);
-  pinMode(BOXSENSORPIN, INPUT);
-  pinMode(PUMPRELAYPIN, OUTPUT);
   pinMode(HEATRELAYPIN, OUTPUT);
   pinMode(FANRELAYPIN, OUTPUT);
-  pinMode(CIRCRELAYPIN, OUTPUT);
-  
-  // ****** Morse Beacon Begins ******
-  pinMode(morseLEDpin, OUTPUT) ;
-  // ****** Morse Beacon Ends ******
+
+  // INITIAL PUMP RUN ON RESET
+  digitalWrite(PUMPRELAYPIN, HIGH);
+  delay(PUMPONTIME);
+  digitalWrite(PUMPRELAYPIN, LOW);
 }
 
-void loop() {
-  moistureSensor = analogRead(MOISTUREPIN);
-  heatSensor = analogRead(TRAYSENSORPIN);
-  airTempSensor = analogRead(BOXSENSORPIN);
-  
-  currentMillis = millis();
+void loop()
+{
 
-// PUMP ACTUATION
+  // Call Update for each instance of the class RelayTimer:
 
-  if (currentMillis > firstWatering) {
-    digitalWrite(PUMPRELAYPIN, HIGH);
-  }
-  if (currentMillis > firstWatering + (WATERPUMPTIME * 1000.)) {
-    firstWatering = OVERFLOWDAYS * dayMs;
-    digitalWrite(PUMPRELAYPIN, LOW);
+  pump.Update();
+
+  // Start light timer after start time delay (stay dark during germination)
+  if(millis() > LIGHTSTARTTIME){
+    light.Update();
   }
 
-  if (currentMillis > secondWatering) {
-    digitalWrite(PUMPRELAYPIN, HIGH);
+  // Read thermistor 10 times and take the average to stabilize reading and reduce relay chatter
+  int average = 0;
+  for (int i=0; i < 10; i++) {
+  average = average + analogRead(THERMISTORPIN);
+  delay(100);
   }
-  if (currentMillis > secondWatering + (WATERPUMPTIME * 1000.)) {
-    secondWatering = OVERFLOWDAYS * dayMs;
-    digitalWrite(PUMPRELAYPIN, LOW);
-  }
-  
-  if (currentMillis > thirdWatering) {
-    digitalWrite(PUMPRELAYPIN, HIGH);
-  }
-  if (currentMillis > thirdWatering + (WATERPUMPTIME * 1000.)) {
-    thirdWatering = OVERFLOWDAYS * dayMs;
-    digitalWrite(PUMPRELAYPIN, LOW);
-  }
-  
-// FAN CONTROL
+  average = average / 10;
 
-  if (airTempSensor > AIRTEMPSET) {
-    fan1Toggle = 1;
-    Serial.println("EXHAUST FAN ON");
-  }
-  else {
-    fan1Toggle = 0;
-    Serial.println("EXHAUST FAN OFF");
-  }
-
-  if (fan1Toggle == 1){
-    digitalWrite(FANRELAYPIN, HIGH);
-  }
-  else {
-    digitalWrite(FANRELAYPIN, LOW);
-  }
-
-// CIRCULATION FAN CONTROL
-
-  if (digitalRead(FANRELAYPIN)) {
-  digitalWrite(CIRCRELAYPIN, LOW);
-  Serial.println("CIRCULATION FAN OFF");
-  }
-  else {
-    if (airTempSensor > AIRTEMPSET - AIRTEMPOFFSET){
-      digitalWrite(CIRCRELAYPIN, HIGH);
-      Serial.println("CIRCULATION FAN ON");
-  }
-  }
-
-// EXHAUST FAN RECURRING OPERATION
-
-  if (currentMillis > recurringFanStart) {
-    digitalWrite(FANRELAYPIN, HIGH);
-    Serial.println ("RECURRING EXHAUST FAN ON");
-    recurringFanStart = currentMillis + EXHAUSTFANCYCLETIME;
-    delay(EXHAUSTFANDELAY);
-    digitalWrite(FANRELAYPIN, LOW);
-  }
-
-// HEAT MAT CONTROL
-
-  if (heatSensor < HEATSET) {
+  // Check thermistor and start heat if below set point
+  if(average < HEATSETPOINT && digitalRead(HEATRELAYPIN) == LOW){
     digitalWrite(HEATRELAYPIN, HIGH);
-    Serial.println("HEAT MATS ON");
-  }
-  else {
+    Serial.print(average); Serial.println(" Heat On ++++++++++++++");
+    }
+  if(average > HEATSETPOINT + HYSTERESIS && digitalRead(HEATRELAYPIN) == HIGH){
     digitalWrite(HEATRELAYPIN, LOW);
-    Serial.println("HEAT MATS OFF");
-  }
-
-// BUTTON SENSOR
-
-  if (digitalRead(BUTTONPIN) == LOW) {
-    digitalWrite(HEATRELAYPIN, LOW);
-    digitalWrite(PUMPRELAYPIN, HIGH);
-    Serial.println("");
-    Serial.print("Manual Engagement of Pump by Button for ");
-    Serial.print(WATERPUMPTIME);
-    Serial.println(" Seconds Run Time");
-    delay(waterPumpTime);
-    digitalWrite(PUMPRELAYPIN, LOW);
-  }
-
-// MOISTURE SENSOR PUMP LOCKOUT
-
-  if (moistureSensor < WETSOILSET) {     // turn off pump if soil is wet
-    digitalWrite(PUMPRELAYPIN, LOW);
-  }
-
-
-    // ****** Morse Beacon Begins ******
-    //char message[] = "1234568"; // worked
-    int morseI = 57; // set integer variable to be read out equal to morseI here
-    char moistureMessage[21]; // enough to hold all numbers up to 64-bits
-    char heat0Message[21]; // enough to hold all numbers up to 64-bits
-    char airTempMessage[21]; // enough to hold all numbers up to 64-bits
-    itoa(moistureSensor, moistureMessage, 10);
-    itoa(heatSensor, heat0Message, 10);
-    itoa(airTempSensor, airTempMessage, 10);
-    Serial.println("Sending Morse");
-//    sendmsg("M ");
-//    sendmsg(moistureMessage) ;
-//    delay(1000);
-    sendmsg("H ");
-    sendmsg(heat0Message);
-    delay(1000);
-    sendmsg("A ");
-    sendmsg(airTempMessage);
-    Serial.println("");
-    //sendmsg("K6HX/B CM87") ;// original
-    delay(1000) ;
-    // ****** Morse Beacon Ends ******
-
-
+    Serial.print(average); Serial.println("Heat Off -------------");
+    }
   
-  Serial.println("*******************************");
-  Serial.println("Arduino Growth Chamber Controller");
-  Serial.print("Days since restart: ");
-  Serial.println(currentMillis / 86400000);
-  /*  
-  Serial.print("Days to First Watering: ");
-  Serial.println((firstWatering - currentMillis) / dayMs);
-  Serial.print("Hours to First Watering: ");
-  Serial.println((firstWatering - currentMillis) / hourMs);
-  Serial.print("Days to Second Watering: ");
-  Serial.println((secondWatering - currentMillis) / dayMs);
-  Serial.print("Hours to Second Watering: ");
-  Serial.println((secondWatering - currentMillis) / hourMs);
-  Serial.print("Moisture Sensor: ");
-  Serial.println(moistureSensor);
-  Serial.print("Heat Sensor: ");
-  Serial.println(heatSensor);
-  Serial.print("HEAT SET TEMP: ");
-  Serial.println(HEATSET);
-  Serial.print("Air Temp Sensor: ");
-  Serial.println(airTempSensor);
-  Serial.print("AIR SET TEMP: ");
-  Serial.println(AIRTEMPSET);
-   */
-  Serial.println("   *   *   *   *   *   ");
-  Serial.print("Tray Temperature in F: ");
-  int heatF = heatSensor/4.34;
-  Serial.println(heatF);
-  Serial.print("Box Temperature in F: ");
-  int airF = airTempSensor / 4.25;
-  Serial.println(airF);
+  // Check thermistor and shut down fan if below set point, start fan if above set point + hysterisis 
+  if(average > HEATSETPOINT + HYSTERESIS && digitalRead(FANRELAYPIN) == LOW){
+    digitalWrite(FANRELAYPIN, HIGH);
+    Serial.print(average); Serial.println("Fan On ----------------");
+  }
+  if(average < HEATSETPOINT + HYSTERESIS - HYSTERESIS && digitalRead(FANRELAYPIN) == HIGH){
+    digitalWrite(FANRELAYPIN, LOW);
+    Serial.print(average); Serial.println("Fan Off");
+  }
+
+  // DEBUG
+  Serial.print(average);
+  if(digitalRead(HEATRELAYPIN)){
+    Serial.println(" HEAT ON");
+  }
+  if(digitalRead(FANRELAYPIN)){
+    Serial.println(" FAN ON");
+  }
+
+  // Check if button has been pressed, if so, run pump
+  if(digitalRead(BUTTONPIN) == LOW){
+    digitalWrite(HEATRELAYPIN, LOW);  // Turn off heat during pump activation
+    digitalWrite(FANRELAYPIN, LOW);   // Turn off fan during pump activation
+    digitalWrite(PUMPRELAYPIN, HIGH); // Run pump for period defined in PUMPONTIME
+    delay(PUMPONTIME);
+    digitalWrite(PUMPRELAYPIN, LOW);
+  }
+  
+//  delay(1000);                         // Loop delay
+
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Arduino Controller for a Microgreen Grow Box, with heater, exhaust vent, internal circulation fan and flood and drain system.
+Arduino Controller for a Microgreen Grow Box, with heater, exhaust vent, lighting, internal circulation fan and flood and drain system.
 
 This system provides a ventilated, temperature controlled box which is secure from insects and rodents, and automates watering of a flood and drain watering system.
 
@@ -7,14 +7,15 @@ Some features not yet tested!
 
 Features include:
 
-Pump can be started up to three times per crop with a timer.
-Pump can be controlled or limited by capacitive moisture sensor.
-Button switch to manually run pump one time.
-Heat Mat Thermostat Control with a thermistor.
-Exhaust fan controled by box air temperature.
-Air circulation fan runs either continuously or when exhaust fan is off.
-Thermistor and moisture sensor values are flashed in morse code.
-Morse can be turned off to speed up cycle loop.
+Flood and Drain Pump can be run at a desired frequency (for example, every six hours), with a specified run duration (for example, 60 seconds).
+
+Button switch allows manual running of the pump through one cycle.
+
+Heat Mats are thermostaticaly controlled with a thermistor. The temperature set point can be programmed.  There is an hysteresis setting to avoid frequent on/off relay activity due to sensor swings.
+
+Exhaust fan and circulation fan are controled by the thermistor, and turn on when the heat mats have reached their maximum setpoint.  The system runs in heating mode, then switches to fan mode, then back to heating mode as the internal temperature rises and falls.  This provides a good amount of ventilation air while maintaining a seed germination and growth temperature ideal for the plant.
+
+The LED strip lighting is controlled to run after a germination phase (for example, five days with no light), and then it will switch to a daily light regimen (for example, 8 hours off, 16 hours on).
 
 This project is still in testing and development.  If successful I intend to provide how-to-build instructions as an open source project.
 
@@ -23,46 +24,25 @@ Build Notes:
 Sensors:
 
 Tray thermistor (nominal 10k ohms) to 3.3V and analog pin 0 with 10k pulldown resistor from pin 0 to ground. 
-Capacitive Soil Moisture Sensor with AOUT to pin 1, positive to 5V, negative to ground.
-Air thermistor (nominal 10k ohms) to 3.3V and analog pin 2 with 10k pulldown resistor from pin 2 to ground. 
 
 Display:
 
-LED on Pin 13 will flash sensor values in morse code
-M indicates moisture sensor value, H indicates tray thermistor value, A indicates box air thermistor value.
-Morse readout can be disabled by commenting out a section of code.  Disabling the morse readout speeds up the cycle loop time of the code.
 
 Relays and Devices:
 
-120V Pump Relay is on Pin 4
-12V Circulation fan relay is Pin 5
-12V Exhaust fan relay is Pin 6
-120V Heat relay is Pin 7
+120V pump relay is on Pin 4
+12V fan relay is Pin 5
+12V LED strip light relay is Pin 6
+120V heat mat relay is Pin 7
 
-Pump Options:
-Pump will run on timer.
-Pump will run on timer, but will only run if soil moisture sensor indicates dry soil.
-Pump will run on timer, but will turn off if soil becomes moist during pumping.
-Pump will run if sensor indicates dry soil (no timer).
-Note: I am currently in testing to determine the best option for watering.
 
-User may change pumpCycleOnDuration to set pump run time (currently 60 seconds)
-
-Ground Pin 12 with a button switch to manually run pump once 
-
-Confirm DRYSOILSET, WETSOILSET, HEATSET values and pump cycle durations prior to use! 
-
-Paul VandenBosch, 20190222
+Paul VandenBosch, 20190421
 
 Suggested Product List (January, 2019):
 
 Arduino Uno Clone: https://www.aliexpress.com/item/1pcs-Smart-Electronics-high-quality-UNO-R3-MEGA328P-CH340G-for-arduino-Compatible-with-USB-CABLE/32803501325.html
 
 Four Relay Shield for Arduino: https://www.aliexpress.com/item/4-Channel-5V-Relay-Swtich-Expansion-Drive-Board-LED-indicator-Relay-Shield-V2-0-Development-Board/32886386019.html
-
-Capacitive Soil Moisture Sensor: https://www.aliexpress.com/item/Capacitive-Soil-Moisture-Sensor-Module-Not-Easy-to-Corrode-Wide-Voltage-Wire-3-3-5-5V/32892951369.html
-
-Note: Capacitive Soil Moisture Sensor must be coated with varnish, epoxy or similar waterproof coating.  I used three coats of varnish.
 
 Thermistor on 2m cable: https://www.aliexpress.com/item/2m-200cm-Two-2-Meter-Waterproof-NTC-Thermistor-Accuracy-Temperature-Sensor-10K-1-3950-Wire-Cable/32904302533.html
 
