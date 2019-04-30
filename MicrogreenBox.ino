8491b6e4e04b7d3f1b19f80540134a81cbda0850
Paul VandenBosch <skyl4rk@gmail.com>
2019-04-29 22:03:16 -0400

    Added morse code flasher readout of temperature sensor value

--- MicrogreenBox.ino
+++ MicrogreenBox.ino
@@ -7,7 +7,7 @@
 #define LIGHTONTIME 57600000     // Run light for X milliseconds - 16 hours is 57600000
 #define LIGHTOFFTIME 28800000    // Pause light for X milliseconds - 8 hours is 28800000
 #define LIGHTSTARTTIME 432000000 // Wait X milliseconds before starting light - 5 days is 432000000
-#define HEATSETPOINT 330         // Thermistor setting below which heater is turned on
+#define HEATSETPOINT 320         // Thermistor setting below which heater is turned on
 #define HYSTERESIS 10            // HEATSETPOINT + HYSTERESIS is when the heat turns off and the fan turns on
 
 #define HEATRELAYPIN 4           // Heat relay pin number
@@ -73,6 +73,120 @@ class RelayTimer
 RelayTimer pump(PUMPRELAYPIN, PUMPONTIME, PUMPOFFTIME);
 RelayTimer light(LIGHTRELAYPIN, LIGHTONTIME, LIGHTOFFTIME);
 
+// Simple Arduino Morse Beacon
+// ****** Morse Beacon Begins ******
+
+#define N_MORSE  (sizeof(morsetab)/sizeof(morsetab[0]))
+#define SPEED  (12)
+#define DOTLEN  (1200/SPEED)
+#define DASHLEN  (4*(1200/SPEED))
+
+int morseLEDpin = 13 ;
+
+struct t_mtab {
+  char c, pat;
+} ;
+
+struct t_mtab morsetab[] = {
+  {'.', 106},
+  {',', 115},
+  {'?', 76},
+  {'/', 41},
+  {'A', 6},
+  {'B', 17},
+  {'C', 21},
+  {'D', 9},
+  {'E', 2},
+  {'F', 20},
+  {'G', 11},
+  {'H', 16},
+  {'I', 4},
+  {'J', 30},
+  {'K', 13},
+  {'L', 18},
+  {'M', 7},
+  {'N', 5},
+  {'O', 15},
+  {'P', 22},
+  {'Q', 27},
+  {'R', 10},
+  {'S', 8},
+  {'T', 3},
+  {'U', 12},
+  {'V', 24},
+  {'W', 14},
+  {'X', 25},
+  {'Y', 29},
+  {'Z', 19},
+  {'1', 62},
+  {'2', 60},
+  {'3', 56},
+  {'4', 48},
+  {'5', 32},
+  {'6', 33},
+  {'7', 35},
+  {'8', 39},
+  {'9', 47},
+  {'0', 63}
+} ;
+
+void
+dash()
+{
+  digitalWrite(morseLEDpin, HIGH) ;
+  delay(DASHLEN);
+  digitalWrite(morseLEDpin, LOW) ;
+  delay(DOTLEN) ;
+}
+
+void
+dit()
+{
+  digitalWrite(morseLEDpin, HIGH) ;
+  delay(DOTLEN);
+  digitalWrite(morseLEDpin, LOW) ;
+  delay(DOTLEN);
+}
+
+void
+send(char c)
+{
+  int i ;
+  if (c == ' ') {
+    Serial.print(c) ;
+    delay(7 * DOTLEN) ;
+    return ;
+  }
+  for (i = 0; i < N_MORSE; i++) {
+    if (morsetab[i].c == c) {
+      unsigned char p = morsetab[i].pat ;
+      Serial.print(morsetab[i].c) ;
+
+      while (p != 1) {
+        if (p & 1)
+          dash() ;
+        else
+          dit() ;
+        p = p / 2 ;
+      }
+      delay(2 * DOTLEN) ;
+      return ;
+    }
+  }
+  /* if we drop off the end, then we send a space */
+  Serial.print("?") ;
+}
+
+void
+sendmsg(char *str)
+{
+  while (*str)
+    send(*str++) ;
+  Serial.println("");
+}
+// ****** Morse Beacon Ends ******
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -85,6 +199,10 @@ void setup()
   digitalWrite(PUMPRELAYPIN, HIGH);
   delay(PUMPONTIME);
   digitalWrite(PUMPRELAYPIN, LOW);
+
+  // ****** Morse Beacon Begins ******
+  pinMode(morseLEDpin, OUTPUT) ;
+  // ****** Morse Beacon Ends ******
 }
 
 void loop()
@@ -148,7 +266,20 @@ void loop()
     delay(PUMPONTIME);
     digitalWrite(PUMPRELAYPIN, LOW);
   }
+
   
+    // ****** Morse Beacon Begins ******
+    //char message[] = "1234568"; // worked
+    int morseI = 57; // set integer variable to be read out equal to morseI here
+    
+    char airTempMessage[21]; // enough to hold all numbers up to 64-bits
+    itoa(average, airTempMessage, 10);
+    Serial.println("Sending Morse");
+    sendmsg("A ");
+    sendmsg(airTempMessage);
+    Serial.println("");
+    // ****** Morse Beacon Ends ******
+    
 //  delay(1000);                         // Loop delay
 
 }
